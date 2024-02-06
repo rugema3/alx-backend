63033a96f4a8c70449f989cdc239ebfde48581cf
rugema3 <rugema61@gmail.com>
2024-02-06 20:03:49 +0200

    . Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -7,6 +7,14 @@ from flask_babel import Babel
 # Initiarize a flask app.
 app = Flask(__name__)
 
+# Mock user database table
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
 
 class Config(object):
     """Configure Babel."""
@@ -19,6 +27,31 @@ app.config.from_object(Config)
 
 # Instantiate the Babel object
 babel = Babel(app)
+
+
+def get_user():
+    """
+    Returns user information based on the 'login_as' parameter in
+    the request URL.
+
+    Returns:
+        dict or None: A dictionary containing user information if 'login_as'
+        parameter is present
+                      and valid, otherwise returns None.
+    """
+    user_id = request.args.get('login_as')
+    if user_id is not None and int(user_id) in users.keys():
+        return users[int(user_id)]
+    return None
+
+
+@app.before_request
+def before_request():
+    """Execute before all other functions."""
+    current_user = get_user()
+    g.current_user = current_user
+    print("Logged in user:", g.current_user)
+    print(f"Name: {g.current_user['name']}")
 
 
 @babel.localeselector
@@ -40,7 +73,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """Home page of our flask application."""
-    return render_template('4-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
